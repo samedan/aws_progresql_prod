eb059cba485321b0428cf8696c4463802f8b1101
Daniel Popescu <same.dan@gmail.com>
2020-03-20 13:43:49 +0100

    Front Page no Data 1

---
+++ build/precache-manifest.a37043dccaf2b75c08f10cbf5f70ee91.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9101d3defb713fb41d79",
+    "url": "/static/css/main.d12dc8e4.chunk.css"
+  },
+  {
+    "revision": "9101d3defb713fb41d79",
+    "url": "/static/js/main.9101d3de.chunk.js"
+  },
+  {
+    "revision": "367bfc52e4585f64815d",
+    "url": "/static/js/1.367bfc52.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5f2a8a34c57e6139141c2f86194cc229",
+    "url": "/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.abb8e81629cb54dc0e3078ed01771938.js"
+  "/precache-manifest.a37043dccaf2b75c08f10cbf5f70ee91.js"
 );
 
 workbox.clientsClaim();
