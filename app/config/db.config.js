6e0e578c24d26d27b712bba2b795dba12e77bc3d
Daniel Popescu <same.dan@gmail.com>
2020-03-21 10:50:46 +0100

    New Front end / Backend

---
+++ app/config/db.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  HOST: 'dbname5.cwn3k4lw9hvz.eu-west-3.rds.amazonaws.com',
+  USER: 'samedan78',
+  PASSWORD: 'Fuckyahoo667',
+  DB: 'dbname5b',
+  dialect: 'postgres',
+  ssl: true,
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+};
+
+//   user: 'samedan78',
+//   host: 'dbname5.cwn3k4lw9hvz.eu-west-3.rds.amazonaws.com',
+//   database: 'dbname5',
+//   password: 'Fuckyahoo667',
+//   post: 5432,
+//   ssl: true
---
+++ build/precache-manifest.cb604d62e620aff6f47538e463d7c1a9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06069a348e8dcd13d35aae88614d5d5f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9c64d4bacc94cdf5d8ac",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "fb360ea7e61e6e805e4c",
+    "url": "/static/css/main.89ff9286.chunk.css"
+  },
+  {
+    "revision": "9c64d4bacc94cdf5d8ac",
+    "url": "/static/js/2.247c9d53.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.247c9d53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb360ea7e61e6e805e4c",
+    "url": "/static/js/main.ef4c6961.chunk.js"
+  },
+  {
+    "revision": "2dc00040666ec5483b89",
+    "url": "/static/js/runtime-main.e028d6aa.js"
+  }
+]);
